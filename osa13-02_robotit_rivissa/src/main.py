d41f9cfab0e40db95175a14f21d363e0f6940923
jani.vaismaa <jani.vaismaa@PF3KN91A>
2022-10-26 11:59:30 +0300

    robotit rivissä tehtävä korjattu

--- osa13-02_robotit_rivissa/src/main.py
+++ osa13-02_robotit_rivissa/src/main.py
@@ -1,1 +1,18 @@
 # TEE RATKAISUSI TÄHÄN:
+import pygame
+ 
+pygame.init()
+naytto = pygame.display.set_mode((640, 480))
+ 
+robo = pygame.image.load("robo.png")
+ 
+naytto.fill((0, 0, 0))
+for i in range(10):
+    naytto.blit(robo, (50+50*i, 100))
+pygame.display.flip()
+ 
+while True:
+    for tapahtuma in pygame.event.get():
+        if tapahtuma.type == pygame.QUIT:
+            exit()
+# TEE RATKAISUSI TÄHÄN:
